2c873228c4ef3c1bb3ea2f97781d636cdac29b57
metalpadawan <endem@byupathway.edu>
2025-09-06 16:48:46 +0100

    more updates

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./lesson1');
-
-app.get('/', lesson1Controller.mangaRoute);
-app.get('/dcu', lesson1Controller.jamesRoute);
 
 const port = 3000;
- 
+
+app.use('/', require('./routes'));
+
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || port));
 });
